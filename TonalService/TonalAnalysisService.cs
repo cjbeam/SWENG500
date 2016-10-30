925a9384375cf3439e5f22bdfd98d893d1d9e91e
dlaustsen <dlaustsen@fairhealth.org>
2016-10-30 14:47:07 -0400

    Adding to Service Example of how to save Analysis Results

--- TonalService/TonalAnalysisService.cs
+++ TonalService/TonalAnalysisService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Diagnostics;
+using Newtonsoft.Json;
 using ToneReader;
 
 namespace TonalService
@@ -12,11 +14,47 @@ namespace TonalService
     {
        public string GetAnalysis(string email, string text)
        {
-           var provider = new AnalysisProvider();
+            var provider = new AnalysisProvider();
             provider.Analyze(email,text);
-           return provider.EmailAnalysis;
+            string jsonResult = provider.EmailAnalysis;
+            PersistAnalysis(email, jsonResult);
+            return jsonResult;
        }
-
+        public void PersistAnalysis(string email, string json)
+        {
+            EmailAnalysis emailAnalysis = JsonConvert.DeserializeObject<EmailAnalysis>(json);
+            PersistResults(emailAnalysis, email);
+        }
+        public void PersistResults(EmailAnalysis emailAnalysis, string emailAddress)
+        {
+            int analysisId = 1; //Get unique value from sequence in SQL Server?
+            Debug.WriteLine(String.Format("INSERT INTO ANALYSIS (ANALYSIS_ID, USER_EMAIL, ANALYSIS_DATE) VALUES ({0},{1},GETDATE()",analysisId, emailAddress));
+            PersistBodyResults(analysisId, emailAnalysis.BodyResult);
+            PersistSentenceResults(analysisId, emailAnalysis.SentenceResult);
+        }
+        public void PersistBodyResults(int analysisKey, EmailAnalysis.BodyAnalysis bodyAnalysis)
+        {
+            foreach (var categoryAnalysis in bodyAnalysis.CategoryAnalyses)
+            {
+                foreach(var categoryScore in categoryAnalysis.Tones)
+                {
+                    Debug.WriteLine(String.Format("INSERT INTO BODY_ANALYSIS (ANALYSIS_ID, CATEGORY, TONE_NAME, SCORE) VALUES ({0},\"{1}\",\"{2}\",{3})", analysisKey, categoryAnalysis.CategoryId, categoryScore.ToneName.Replace("_big5",""), categoryScore.Score));
+                }
+            }
+        }
+        public void PersistSentenceResults(int analysisKey, List<EmailAnalysis.SentenceAnalysis> sentenceAnalysisList)
+        {
+            foreach (var sentenceAnalysis in sentenceAnalysisList)
+            {
+                foreach (var categoryAnalysis in sentenceAnalysis.CategoryAnalyses)
+                {
+                    foreach (var categoryScore in categoryAnalysis.Tones)
+                    {
+                        Debug.WriteLine(String.Format("INSERT INTO SENTENCE_ANALYSIS (ANALYSIS_ID, SENTENCE_ID, CATEGORY, TONE_NAME, SCORE) VALUES (\"{0}\",{1},\"{2}\",\"{3}\",{4})", analysisKey, sentenceAnalysis.SentenceId, categoryAnalysis.CategoryId,  categoryScore.ToneName.Replace("_big5", ""), categoryScore.Score));
+                    }
+                }
+            }
+        }
 
     }
 }
---
+++ TonalUnitTesting/TonalAnalysisServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TonalService;
+using ToneAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TonalService.Tests
+{
+    [TestClass()]
+    public class TonalAnalysisServiceTests
+    {
+        [TestMethod()]
+        public void GetAnalysisTest()
+        {
+            TonalService.TonalAnalysisService service = new TonalService.TonalAnalysisService();
+            string results = service.GetAnalysis("DLaustsen@fairhealth.org", "We, therefore, the Representatives of the united States of America, in General Congress, Assembled, appealing to the Supreme Judge of the world for the rectitude of our intentions, do, in the Name, and by Authority of the good People of these Colonies, solemnly publish and declare, That these united Colonies are, and of Right ought to be Free and Independent States, that they are Absolved from all Allegiance to the British Crown, and that all political connection between them and the State of Great Britain, is and ought to be totally dissolved; and that as Free and Independent States, they have full Power to levy War, conclude Peace, contract Alliances, establish Commerce, and to do all other Acts and Things which Independent States may of right do. — And for the support of this Declaration, with a firm reliance on the protection of Divine Providence, we mutually pledge to each other our Lives, our Fortunes, and our sacred Honor.");
+            Console.WriteLine(results);
+            Assert.IsTrue(true);
+        }
+    }
+}
