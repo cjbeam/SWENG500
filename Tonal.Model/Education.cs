3d1086dcb963018cfbe720fb13e29c5c83e529e2
knang <whois.kanel@gmail.com>
2016-10-30 23:22:49 -0400

    Registration Cloud Service Registration Data Service

---
+++ Tonal.Model/Education.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonal.Model
+{
+    public class Education
+    {
+        public int EducationId { get; set; }
+        public string EducationLevel { get; set; }
+    }
+}
---
+++ WCFMemberServiceWebRole/IMemberService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFMemberServiceWebRole
+{
+    [ServiceContract]
+    public interface IMemberService
+    {
+        [OperationContract]
+        Tonal.Model.Member Read(string email);
+
+        [OperationContract]
+        bool Create(Tonal.Model.Member member);
+    }
+}
