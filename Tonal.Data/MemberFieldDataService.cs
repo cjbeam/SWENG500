053a2577314975aff0b438324e9a759fd110e9d6
knang <whois.kanel@gmail.com>
2016-11-10 23:32:50 -0500

    fix for utf added factory to header in member service

---
+++ Tonal.Data/MemberFieldDataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonal.Data
+{
+    public abstract class AbstractMemberFieldDataService : BaseDataService
+    {
+        abstract public DataTable LookupEducationOptions();
+        abstract public DataTable LookupGenderOptions();
+        abstract public DataTable LookupStateOptions();
+    }
+
+    public class MemberFieldDataService : AbstractMemberFieldDataService
+    {
+        public override DataTable LookupEducationOptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable LookupGenderOptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable LookupStateOptions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
