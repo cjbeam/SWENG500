76c20483c768e096e21287bbcd4fd934ef6523ca
knang <whois.kanel@gmail.com>
2016-11-12 14:07:07 -0500

    list options for registration svc

---
+++ WCFMemberServiceWebRole/IMemberFieldService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFMemberServiceWebRole
+{
+    [ServiceContract]
+    public interface IMemberFieldService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetEducationLevels/")]
+        List<Tonal.Model.Education> GetEducationLevels();
+        
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetGenderOptions/")]
+        List<Tonal.Model.Gender> GetGenderOptions();
+    
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetStatesList/")]
+        List<Tonal.Model.State> GetStatesList();
+    }
+}
---
+++ WCFMemberServiceWebRole/MemberFieldService.svc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using Tonal.Model;
+
+namespace WCFMemberServiceWebRole
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class MemberFieldService : IMemberFieldService
+    {
+        public List<Education> GetEducationLevels()
+        {
+            Tonal.Data.MemberFieldDataService ds = new Tonal.Data.MemberFieldDataService();
+            var dt = ds.LookupEducationOptions();
+
+            List<Education> list = null;
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                list = new List<Education>();
+                foreach (DataRow item in dt.Rows)
+                {
+                    Education listItem = new Education()
+                    {
+                        EducationId = (int)item["educationId"],
+                        EducationLevel = (string)item["educationLevel"]
+                    };
+
+                    list.Add(listItem);
+                }
+            }
+
+            return list;
+        }
+
+        public List<Gender> GetGenderOptions()
+        {
+            Tonal.Data.MemberFieldDataService ds = new Tonal.Data.MemberFieldDataService();
+            var dt = ds.LookupEducationOptions();
+
+            List<Gender> list = null;
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                list = new List<Gender>();
+                foreach (DataRow item in dt.Rows)
+                {
+                    Gender listItem = new Gender()
+                    {
+                        GenderId = (int)item["genderId"],
+                        GenderType = (string)item["genderType"]
+                    };
+
+                    list.Add(listItem);
+                }
+            }
+
+            return list;
+        }
+
+        public List<State> GetStatesList()
+        {
+            Tonal.Data.MemberFieldDataService ds = new Tonal.Data.MemberFieldDataService();
+            var dt = ds.LookupEducationOptions();
+
+            List<State> list = null;
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                list = new List<State>();
+                foreach (DataRow item in dt.Rows)
+                {
+                    State listItem = new State()
+                    {
+                        StateId = (int)item["stateId"],
+                        StateCode = (string)item["stateName"],
+                        StateName = (string)item["stateCode"]
+                    };
+
+                    list.Add(listItem);
+                }
+            }
+
+            return list;
+        }
+    }
+}
