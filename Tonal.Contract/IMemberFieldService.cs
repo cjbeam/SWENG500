698cc31913ca557bd54fb46bdb73ea89c58afdab
knang <whois.kanel@gmail.com>
2016-11-12 18:58:36 -0500

    architecture cleanup

--- Tonal.Contract/IMemberFieldService.cs
+++ Tonal.Contract/IMemberFieldService.cs
@@ -6,7 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace WCFMemberServiceWebRole
+namespace Tonal.Contract
 {
     [ServiceContract]
     public interface IMemberFieldService
