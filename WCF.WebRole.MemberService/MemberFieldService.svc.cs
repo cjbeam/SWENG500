bb8d665367580737e168e4f177b9a31f07a47029
knang <whois.kanel@gmail.com>
2016-11-13 19:02:06 -0500

    remade new web role wcf

---
+++ WCF.WebRole.MemberService/MemberFieldService.svc.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Tonal.Model;
+
+namespace WCF.WebRole.MemberService
+{
+    [ServiceBehavior(AddressFilterMode = AddressFilterMode.Any)]
+    public class MemberFieldService : Tonal.Contract.IMemberFieldService
+    {
+        public string GetEducationLevels()
+        {
+            Tonal.Data.MemberFieldDataService ds = new Tonal.Data.MemberFieldDataService();
+            var dt = ds.LookupEducationOptions();
+
+            List<Education> list = null;
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                list = new List<Education>();
+                foreach (DataRow item in dt.Rows)
+                {
+                    Education listItem = new Education()
+                    {
+                        EducationId = (int)item["educationId"],
+                        EducationLevel = (string)item["educationLevel"]
+                    };
+
+                    list.Add(listItem);
+                }
+            }
+
+            return JsonConvert.SerializeObject(list);
+        }
+
+        public string GetGenderOptions()
+        {
+            Tonal.Data.MemberFieldDataService ds = new Tonal.Data.MemberFieldDataService();
+            var dt = ds.LookupGenderOptions();
+
+            List<Gender> list = null;
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                list = new List<Gender>();
+                foreach (DataRow item in dt.Rows)
+                {
+                    Gender listItem = new Gender()
+                    {
+                        GenderId = (int)item["genderId"],
+                        GenderType = (string)item["genderType"]
+                    };
+
+                    list.Add(listItem);
+                }
+            }
+
+            return JsonConvert.SerializeObject(list);
+        }
+
+        public string GetStatesList()
+        {
+            Tonal.Data.MemberFieldDataService ds = new Tonal.Data.MemberFieldDataService();
+            var dt = ds.LookupStateOptions();
+
+            List<State> list = null;
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                list = new List<State>();
+                foreach (DataRow item in dt.Rows)
+                {
+                    State listItem = new State()
+                    {
+                        StateId = (int)item["stateId"],
+                        StateCode = (string)item["stateName"],
+                        StateName = (string)item["stateCode"]
+                    };
+
+                    list.Add(listItem);
+                }
+            }
+
+            return JsonConvert.SerializeObject(list);
+        }
+    }
+}
