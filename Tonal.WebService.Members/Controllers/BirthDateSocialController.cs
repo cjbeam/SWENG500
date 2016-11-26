9d5ac976adad54ba102c28cdebec9f4e751c3c5c
knang <whois.kanel@gmail.com>
2016-11-26 13:25:35 -0500

    birthdate analysis api

---
+++ Tonal.WebService.Members/Controllers/BirthDateSocialController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Tonal.WebService.Members.Controllers
+{
+    [System.Web.Http.Cors.EnableCors("*", "*", "*")]
+    public class BirthDateSocialController : ApiController
+    {
+        [Route("api/education/social")]
+        public IHttpActionResult Get()
+        {
+            IHttpActionResult actionResult;
+
+            Tonal.Data.AnalysisDataService ds = new Tonal.Data.AnalysisDataService();
+            var dt = ds.GetAnalysis(Model.Category.social_tone.ToString(), (int)Model.Demographic.birthDate);
+
+            var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(dt);
+
+            if (jsonData != null)
+            {
+                actionResult = Ok(jsonData);
+            }
+            else
+            {
+                actionResult = StatusCode(HttpStatusCode.NotFound);
+            }
+
+            return actionResult;
+        }
+    }
+}
